de5c438b9d283fcc41940e1099572789845fbc86
Patrick <pat.rodriguez108@gmail.com>
2018-02-15 15:10:26 -0600

    Solve algo

--- number_digit_one.rb
+++ number_digit_one.rb
@@ -4,7 +4,17 @@
 # increment by one for each occurence of one
 
 def count_digit_one(n)
-	
+	n_range = (1..n).to_a
+	ones_amount = 0
+	n_range.each do |numbers|
+		digits = numbers.to_s.chars
+		digits.each do |digit|
+			if digit == "1"
+				ones_amount += 1
+			end
+		end
+	end
+	ones_amount
 end
 
 p count_digit_one(13)
